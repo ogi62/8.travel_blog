3cfde7b547f3b1876e79894cf256e37bc52673f6
Ogi62 <danguba962@gmail.com>
2022-10-16 20:38:14 +0200

    pipe for throwing error

--- src/app/shared/service/tourist-blogs.service.ts
+++ src/app/shared/service/tourist-blogs.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { TouristPost } from '../model/post.model';
 
 @Injectable({
@@ -17,6 +17,14 @@ export class TouristBlogsService {
   constructor(private http: HttpClient) { }
 
   getPosts(): Observable<TouristPost[]> {
-    return this.http.get<TouristPost[]>(this.apiurl);
+    return this.http.get<TouristPost[]>(this.apiurl).pipe(
+      tap(data => console.log(data)),
+      catchError(this.handleError)
+    );
+  };
+
+  private handleError(error: any) {
+    console.log(error);
+    return throwError(error);
   };
 }
