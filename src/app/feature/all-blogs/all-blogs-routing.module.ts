c2cdede548214177c4c697e398f21f9beac3990a
Ogi62 <danguba962@gmail.com>
2022-10-17 15:17:14 +0200

    fetch only one city, loading spinner and unsubscribing to prevent memory leak

--- src/app/feature/all-blogs/all-blogs-routing.module.ts
+++ src/app/feature/all-blogs/all-blogs-routing.module.ts
@@ -21,6 +21,11 @@ const routes: Routes = [
         path: '',
         redirectTo: 'all_blogs',
         pathMatch: 'full'
+      },
+      {
+        path: '**',
+        redirectTo: 'all_blogs',
+        pathMatch: 'full'
       }
     ],
   },
--- src/app/feature/all-blogs/components/single-tourist-blog/single-tourist-blog.component.ts
+++ src/app/feature/all-blogs/components/single-tourist-blog/single-tourist-blog.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {  Subject, takeUntil } from 'rxjs';
+import { TouristPost } from 'src/app/shared/model/post.model';
+import { TouristBlogsService } from 'src/app/shared/service/tourist-blogs.service';
 
 @Component({
   selector: 'app-single-tourist-blog',
   templateUrl: './single-tourist-blog.component.html',
   styleUrls: ['./single-tourist-blog.component.scss']
 })
-export class SingleTouristBlogComponent implements OnInit {
+export class SingleTouristBlogComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  singleBlog!: TouristPost;
+  id: any;
+  unsubscribe: Subject<void> = new Subject<void>();
+  loading = false;
+  constructor(
+    private touristService: TouristBlogsService,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.id = this.route.snapshot.paramMap.get('id');
+    if(this.id) {
+      this.touristService.getPost(this.id).pipe(takeUntil(this.unsubscribe)).subscribe((data: TouristPost)=> {
+        console.log(data);
+        this.singleBlog = data;
+        this.loading = false;
+      })
+    }
+    
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
